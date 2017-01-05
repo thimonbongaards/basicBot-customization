b53654a2efe094d46e7c712ce15c241c9e8a1de8
thimonbongaards <thimonbongaards1@gmail.com>
2017-01-05 18:20:13 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -55,7 +55,7 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "basicBot",
+        botName: "FlimeBOT",
         language: "english",
         chatLink: "https://rawgit.com/" + fork + "/basicBot/master/lang/en.json",
         maximumAfk: 120,
