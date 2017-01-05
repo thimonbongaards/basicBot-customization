ed1fdb1826c4aaaca3ca1be05f2a8c6c0b7e9ed4
thimonbongaards <thimonbongaards1@gmail.com>
2017-01-05 18:55:56 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 (function () {
     //Link location of your fork so you don't have to modify so many things.
-    var fork = "thimonbonaard";
+    var fork = "thimonbonaards";
 		
     //Define our function responsible for extending the bot.
     function extend() {
